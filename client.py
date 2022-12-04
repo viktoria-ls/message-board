1f0cd5044115fb0a66442a885e83d9723939a072
Viktoria Lila Sicuan <viktoria.sicuan@gmail.com>
2022-12-05 00:10:42 +0800

    Finished user input and connection establishment, working on server replies

--- client.py
+++ client.py
@@ -1,22 +1,11 @@
 import socket
 import json
 
-# client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# buffer_size = 1024
-
-# # this is server data that we want to connect to
-# # should be inputted by client user
-# host_ip = socket.gethostbyname(socket.gethostname())
-# port = 2468
-
-# print("client host: " + host_ip)
-
-# msg = input("Message: ")
-# encoded_msg = str.encode(msg)
-
-# client_sock.sendto(encoded_msg, (host_ip, port))
-# reply = client_sock.recvfrom(buffer_size)
-# print(reply)
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+buffer_size = 1024
+server_ip = None
+port = None
+handle = ""
 
 commands_params = {
     "join": ["server_ip_add", "port"],
@@ -26,55 +15,73 @@ commands_params = {
     "msg": ["handle", "message"]
 }
 
-server_ip = None
-port = None
-running = True
+update = ""
 
-# Refactor to handle errors in server app not here
+def update_board(message):
+    update = message
+    print(update)
+
+def handle_reply(message):
+    print(message)
+
+def send_command(message):
+    encoded_msg = str.encode(json.dumps(message))
+    print(f"SENDING >> {encoded_msg}")
+
+    if server_ip is None and command != "join":
+        update_board("Error: Not connected to a server. Please connect to a server first.")
+    else:
+        client_sock.sendto(encoded_msg, (server_ip, port))
+        handle_reply(client_sock.recvfrom(buffer_size))
+
+running = True
 while running:
     user_input = input("Enter command: ").strip()
     msg_to_server = {}
-    msg_to_server["command"] = "error"
     command = user_input.partition(" ")[0]
 
     # Command doesn't start with '/'
     if not command.startswith('/'):
-        msg_to_server["message"] = "Command not found. Command must start with '/'."
+        update_board("Error: Command not found. Command must start with '/'.")
     else:
         command = command.lstrip('/')
         # Command doesn't exist
         if command not in list(commands_params.keys()):
-            msg_to_server["message"] = "Command not found."
+            update_board("Error: Command not found.")
         else:
-            # Attempting to send command when not connected to a server
-            if server_ip is None and command != "join":
-                msg_to_server["message"] = "Not connected to a server. Please connect to a server first."
-            else:
-                msg_to_server["command"] = command
-                params = user_input.split()[1:]
-                if command == "msg":
-                    msg_to_server["handle"] = params[0]
-                    msg_to_server["message"] = " ".join(params[1:])
-                elif command == "all":
-                    msg_to_server["message"] = " ".join(params)
-                elif command == "leave":
+            msg_to_server["command"] = command
+            params = user_input.split()[1:]
+            if command == "msg":
+                msg_to_server["handle"] = params[0]
+                msg_to_server["message"] = " ".join(params[1:])
+            elif command == "all":
+                msg_to_server["message"] = " ".join(params)
+            elif command == "leave":
+                if server_ip == None:
+                    update_board("Error: Disconnection failed. Please connect to the server first.")
+                else:
+                    send_command(msg_to_server)
                     server_ip = None
                     port = None
+                    handle = ""
+                continue
+            else:
+                # Number of inputted parameters is not allowable
+                if len(params) != len(commands_params[command]):
+                    update_board("Error: Command parameters do not match or is not allowed.")
                 else:
-                    # Number of inputted parameters is not allowable
-                    if len(params) != len(commands_params[command]):
-                        msg_to_server["message"] = "Command parameters do not match or is not allowed."
-                    else:
-                        for index, param in enumerate(commands_params[command]):
-                            msg_to_server[param] = params[index]
-                        if command == "join":
-                            server_ip = params[0]
-                            port = params[1]
-                            # connect to server here and add error message
-                            print("Pretend we're connected to a server!")
+                    for index, param in enumerate(commands_params[command]):
+                        msg_to_server[param] = params[index]
+                    if command == "join":
+                        server_ip = params[0]
+                        port = int(params[1])
+                        try:
+                            send_command(msg_to_server)
+                        except:
+                            update_board("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")   
+                        finally:
+                            continue
+                    elif command == "register":
+                        handle = params[0]
 
-    print(msg_to_server)
-    # encode
-    # send to server
-    # receive response from server (in JSON)
-    # display response
+    send_command(msg_to_server)
--- server.py
+++ server.py
@@ -1,20 +1,51 @@
 import socket
+import json
 
 host_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 host = socket.gethostname()
 port = 2468
 buffer_size = 1024
-
 host_sock.bind((host, port))
+
+commands_params = {
+    "join": [],
+    "register": ["handle"],
+    "all": ["message"],
+    "msg": ["handle", "message"]
+}
+
+users = {}
+
+def send_response(message, ip_port):
+    if message["command"] == "msg":
+        host_sock.sendto(str.encode(json.dumps(message)), users[message[handle]])
+        host_sock.sendto(str.encode(json.dumps(message)), ip_port)
+    elif message["command"] == "all":
+        for add in users.values():
+            host_sock.sendto(str.encode(json.dumps(message)), add)
+    else:
+        host_sock.sendto(str.encode(json.dumps(message)), ip_port)
 
 running = True
 while running:
     print("Waiting for client...")
     data, client_addr = host_sock.recvfrom(buffer_size)
-    if data.lower() == "hello":
-        reply = str.encode("Hello right back at ya!")
-    else:
-        reply = str.encode("Not even a hello?")
-    print(f"Message from [{client_addr}]: {data}")
-    host_sock.sendto(reply, client_addr)
+    data = json.loads(data.decode())
+    print(data)
+
+    msg_to_client = data
+
+    if data["command"] == "register":
+        if data["handle"] in list(users.keys()):
+            msg_to_client["command"] = "error"
+            msg_to_client["message"] = "Error: Registration failed. Handle or alias already exists."
+        else:
+            users[data["handle"]] = client_addr
+    elif data["command"] == "msg":
+        if data["handle"] not in list(users.keys()):
+            msg_to_client["command"] = "error"
+            msg_to_client["message"] = "Error: Handle or alias not found."
+    elif data["command"] == "all":
+        msg_to_client["sender"] = list(users.keys())[list(users.values()).index(client_addr)]
+
+    send_response(msg_to_client, client_addr)
