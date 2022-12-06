ed2e99db39387af1eefc6c8870de64c6f226c690
Viktoria Lila Sicuan <viktoria.sicuan@gmail.com>
2022-12-06 09:59:52 +0800

    Implemented threading for sync listening and outputting

--- client.py
+++ client.py
@@ -1,14 +1,21 @@
 import socket
 import json
+import queue
+import threading
 
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip = "127.0.0.1"
+port = 0
 buffer_size = 1024
+client_sock.bind((ip, port))
+
+messages = queue.Queue()
 server_ip = None
-port = None
+server_port = None
 handle = ""
 
 commands_params = {
-    "join": ["server_ip_add", "port"],
+    "join": ["server_ip_add", "server_port"],
     "leave": [],
     "register": ["handle"],
     "all": ["message"],
@@ -22,107 +29,120 @@ def update_board(message):
     update = message
     print(update)
 
-def handle_reply(message):
-    global handle
-    data, server_addr = message
-    data = json.loads(data.decode())
-    print(f"RECEIVING >> {data}")
-    command = data["command"]
+def handle_reply():
+    while True:
+        message = client_sock.recvfrom(buffer_size)
+        messages.put(message)
+        while not messages.empty():
+            global handle
+            
+            data, server_addr = messages.get()
+            data = json.loads(data.decode())
+            # print(f"RECEIVING >> {data}")
+            command = data["command"]
 
-    if command == "join":
-        update_board("Connection to the Message Board Server is successful!")
-    elif command == "leave":
-        update_board("Connection closed. Thank you!")
-    elif command == "register":
-        update_board(f"Welcome {data['handle']}!")
-        handle = data["handle"]
-    elif command == "all":
-        update_board(f"{data['sender']}: {data['message']}")
-    elif command == "msg":
-        if data["sender"] == handle:
-            update_board(f"(To {data['handle']}): {data['message']}")
-        else:
-            update_board(f"(From {data['sender']}): {data['message']}")
+            if command == "join":
+                update_board("Connection to the Message Board Server is successful!")
+            elif command == "leave":
+                update_board("Connection closed. Thank you!")
+            elif command == "register":
+                update_board(f"Welcome {data['handle']}!")
+                handle = data["handle"]
+            elif command == "all":
+                update_board(f"{data['sender']}: {data['message']}")
+            elif command == "msg":
+                if data["sender"] == handle:
+                    update_board(f"(To {data['handle']}): {data['message']}")
+                else:
+                    update_board(f"(From {data['sender']}): {data['message']}")
 
 def send_command(message):
     global server_ip
-    global port
+    global server_port
 
     encoded_msg = str.encode(json.dumps(message))
     # print(f"SENDING >> {encoded_msg}")
 
     try:
-        port = int(port)
-        client_sock.sendto(encoded_msg, (server_ip, port))
+        server_port = int(server_port)
+        client_sock.sendto(encoded_msg, (server_ip, server_port))
     except:
         server_ip = None
-        port = None
+        server_port = None
         update_board("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")   
-    else:
-        reply = client_sock.recvfrom(buffer_size)
-        handle_reply(reply)
 
-running = True
-while running:
-    user_input = input("Enter command: ").strip()
-    msg_to_server = {}
-    command = user_input.partition(" ")[0]
+def get_input():
+    global server_ip
+    global server_port
+    global handle
+    
+    running = True
+    while running:
+        user_input = input().strip()
+        msg_to_server = {}
+        command = user_input.partition(" ")[0]
 
-    # Command doesn't start with '/'
-    if not command.startswith('/'):
-        update_board("Error: Command not found. Command must start with '/'.")
-        continue
-
-    command = command.lstrip('/')
-
-    # Command doesn't exist
-    if command not in list(commands_params.keys()):
-        update_board("Error: Command not found.")
-        continue
-
-    msg_to_server["command"] = command
-    params = user_input.split()[1:]
-
-    if command == "msg":
-        msg_to_server["handle"] = params[0]
-        msg_to_server["message"] = " ".join(params[1:])
-    elif command == "all":
-        msg_to_server["message"] = " ".join(params)
-    elif command == "?":
-        update_board("Display all commands!!")
-        continue
-    elif command == "leave":
-        if server_ip == None:
-            update_board("Error: Disconnection failed. Please connect to the server first.")
-        else:
-            send_command(msg_to_server)
-            server_ip = None
-            port = None
-            handle = ""
-        continue
-    # Sending command but not connected to server
-    elif server_ip is None and command != "join":
-        update_board("Error: Not connected to a server. Please connect to a server first.")
-        continue
-
-    else:
-        # Number of inputted parameters is not allowable
-        if len(params) != len(commands_params[command]):
-            update_board("Error: Command parameters do not match or is not allowed.")
+        # Command doesn't start with '/'
+        if not command.startswith('/'):
+            update_board("Error: Command not found. Command must start with '/'.")
             continue
 
-        for index, param in enumerate(commands_params[command]):
-            msg_to_server[param] = params[index]
-        if command == "join":
-            if server_ip != None:
-                update_board("Error: You are already connected to a server. Leave first before joining another server.")
-                continue
+        command = command.lstrip('/')
+
+        # Command doesn't exist
+        if command not in list(commands_params.keys()):
+            update_board("Error: Command not found.")
+            continue
+
+        msg_to_server["command"] = command
+        params = user_input.split()[1:]
+
+        if command == "msg":
+            msg_to_server["handle"] = params[0]
+            msg_to_server["message"] = " ".join(params[1:])
+        elif command == "all":
+            msg_to_server["message"] = " ".join(params)
+        elif command == "?":
+            update_board("Display all commands!!")
+            continue
+        elif command == "leave":
+            if server_ip == None:
+                update_board("Error: Disconnection failed. Please connect to the server first.")
             else:
-                server_ip = params[0]
-                port = params[1]
-        elif command == "register":
-            if handle != "":
-                update_board(f'Error: You are already registered under the handle/alias, "{handle}".')
+                send_command(msg_to_server)
+                server_ip = None
+                server_port = None
+                handle = ""
+            continue
+        # Sending command but not connected to server
+        elif server_ip == None and command != "join":
+            update_board("Error: Not connected to a server. Please connect to a server first.")
+            continue
+
+        else:
+            # Number of inputted parameters is not allowable
+            if len(params) != len(commands_params[command]):
+                update_board("Error: Command parameters do not match or is not allowed.")
                 continue
 
-    send_command(msg_to_server)
+            for index, param in enumerate(commands_params[command]):
+                msg_to_server[param] = params[index]
+            if command == "join":
+                if server_ip != None:
+                    update_board("Error: You are already connected to a server. Leave first before joining another server.")
+                    continue
+                else:
+                    server_ip = params[0]
+                    server_port = params[1]
+            elif command == "register":
+                if handle != "":
+                    update_board(f'Error: You are already registered under the handle/alias, "{handle}".')
+                    continue
+
+        send_command(msg_to_server)
+
+t1 = threading.Thread(target=get_input)
+t2 = threading.Thread(target=handle_reply)
+
+t1.start()
+t2.start()
--- server.py
+++ server.py
@@ -1,56 +1,69 @@
 import socket
 import json
+import queue
+import threading
 
 host_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = "127.0.0.1"
+ip = "127.0.0.1"
 port = 12345
 buffer_size = 1024
-host_sock.bind((host, port))
+host_sock.bind((ip, port))
 
+messages = queue.Queue()
 users = {}
 
-def send_response(message, ip_port):
-    if message["command"] == "msg":
-        host_sock.sendto(str.encode(json.dumps(message)), users[message["handle"]])
-        host_sock.sendto(str.encode(json.dumps(message)), ip_port)
-    elif message["command"] == "all":
-        for add in list(users.values()):
-            host_sock.sendto(str.encode(json.dumps(message)), add)
-    else:
-        host_sock.sendto(str.encode(json.dumps(message)), ip_port)
+def send_response():
+    while True:
+        while not messages.empty():
+            message, ip_port = messages.get()
+            if message["command"] == "msg":
+                host_sock.sendto(str.encode(json.dumps(message)), users[message["handle"]])
+                host_sock.sendto(str.encode(json.dumps(message)), ip_port)
+            elif message["command"] == "all":
+                for add in list(users.values()):
+                    host_sock.sendto(str.encode(json.dumps(message)), add)
+            else:
+                host_sock.sendto(str.encode(json.dumps(message)), ip_port)
 
-running = True
-while running:
-    print("Waiting for client...")
-    data, client_addr = host_sock.recvfrom(buffer_size)
-    data = json.loads(data.decode())
-    print(data)
+def receive():
+    running = True
+    while running:
+        print("Waiting for client...")
+        data, client_addr = host_sock.recvfrom(buffer_size)
+        data = json.loads(data.decode())
+        print(data)
 
-    msg_to_client = {}
-    command = data["command"]
-    msg_to_client["command"] = command
+        msg_to_client = {}
+        command = data["command"]
+        msg_to_client["command"] = command
 
-    if command == "register":
-        if data["handle"] in list(users.keys()):
-            msg_to_client["command"] = "error"
-            msg_to_client["message"] = "Error: Registration failed. Handle or alias already exists."
-        else:
-            users[data["handle"]] = client_addr
-            msg_to_client["handle"] = data["handle"]
-    elif command == "msg" or command == "all":
-        if client_addr not in list(users.values()):
-            msg_to_client["command"] = "error"
-            msg_to_client["message"] = "Error: You must be registered with a handle or alias first."
-        elif command == "msg" and data["handle"] not in list(users.keys()):
-            msg_to_client["command"] = "error"
-            msg_to_client["message"] = "Error: Handle or alias not found."
-        else:
-            if command == "msg":
+        if command == "register":
+            if data["handle"] in list(users.keys()):
+                msg_to_client["command"] = "error"
+                msg_to_client["message"] = "Error: Registration failed. Handle or alias already exists."
+            else:
+                users[data["handle"]] = client_addr
                 msg_to_client["handle"] = data["handle"]
-            msg_to_client["message"] = data["message"]
-            msg_to_client["sender"] = list(users.keys())[list(users.values()).index(client_addr)]
-    elif command == "leave":
-        if client_addr in list(users.values()):
-            users.pop(list(users.keys())[list(users.values()).index(client_addr)])
- 
-    send_response(msg_to_client, client_addr)
+        elif command == "msg" or command == "all":
+            if client_addr not in list(users.values()):
+                msg_to_client["command"] = "error"
+                msg_to_client["message"] = "Error: You must be registered with a handle or alias first."
+            elif command == "msg" and data["handle"] not in list(users.keys()):
+                msg_to_client["command"] = "error"
+                msg_to_client["message"] = "Error: Handle or alias not found."
+            else:
+                if command == "msg":
+                    msg_to_client["handle"] = data["handle"]
+                msg_to_client["message"] = data["message"]
+                msg_to_client["sender"] = list(users.keys())[list(users.values()).index(client_addr)]
+        elif command == "leave":
+            if client_addr in list(users.values()):
+                users.pop(list(users.keys())[list(users.values()).index(client_addr)])
+
+        messages.put((msg_to_client, client_addr))
+
+t1 = threading.Thread(target=receive)
+t2 = threading.Thread(target=send_response)
+
+t1.start()
+t2.start()
